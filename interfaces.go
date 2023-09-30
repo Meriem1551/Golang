6b03dc64252f2424e8e4fd40470a2f555ccef06f
Meriem1551 <meriem200409@gmail.com>
2023-09-30 19:18:45 +0200

    Interfaces

---
+++ interfaces.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// shape interface
+type shape interface {
+	area() float64
+	circumf() float64
+}
+
+type square struct {
+	length float64
+}
+type circle struct {
+	radius float64
+}
+
+// square methods
+func (s square) area() float64 {
+	return s.length * s.length
+}
+func (s square) circumf() float64 {
+	return s.length * 4
+}
+
+// circle methods
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+func (c circle) circumf() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func printShapeInfo(s shape) {
+	fmt.Printf("area of %T is: %0.2f \n", s, s.area())
+	fmt.Printf("circumference of %T is: %0.2f \n", s, s.circumf())
+}
