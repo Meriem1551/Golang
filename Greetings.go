0f8a4429beb9863d2d423947ff5611df2b9bdbd5
Meriem1551 <meriem200409@gmail.com>
2023-09-30 12:04:13 +0200

    sharing data between files

---
+++ Greetings.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+var points = []int{20, 30, 90, 100, 12}
+
+func sayHello(n string) {
+	fmt.Println("Hello", n)
+}
+
+func showScore() {
+	fmt.Println("You scored this many points", score)
+}
