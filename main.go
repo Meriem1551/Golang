9071f6130f05b6421152e489de4e73f9c22de774
Meriem1551 <meriem200409@gmail.com>
2023-09-30 19:20:30 +0200

    first commit

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ import "fmt"
 
 //import "fmt" //package from go standard library
 
+var score = 86.8
+
 func main() { //we just have one main func
 	//variables()
 	//fmtMeth()
@@ -19,6 +21,46 @@ func main() { //we just have one main func
 		fmt.Printf("%0.2f", a)
 	*/
 
-	fn, sn := getInitials("Gojo Gito")
-	fmt.Println(fn, sn)
+	/*fn, sn := getInitials("Gojo Gito")
+	fmt.Println(fn, sn)*/
+
+	// Package scope
+	/*
+		sayHello("Meriem")
+		for _, v := range points {
+			fmt.Println(v)
+		}
+
+		showScore()
+	*/
+	//Map()
+	//groupA()
+	//groupB()
+	//pointers()
+
+	//myBill := newBill("Gojo's bill")
+
+	//myBill.updatedTip(10)
+	//myBill.addItem("Sushi", 7.99)
+	//myBill.addItem("Ramen", 8.99)
+	//myBill.addItem("Curry", 5.50)
+	//myBill.addItem("Dorayaki", 1.00)
+	//fmt.Println(myBill.format())
+
+	//myBill := createBill()
+	//fmt.Println(myBill)
+	//promptOptions(myBill)
+
+	shapes := []shape{
+		square{length: 15.2},
+		circle{radius: 7.5},
+		circle{radius: 12.3},
+		square{length: 4.9},
+	}
+
+	for _, v := range shapes {
+		printShapeInfo(v)
+		fmt.Println("---")
+	}
+
 }
